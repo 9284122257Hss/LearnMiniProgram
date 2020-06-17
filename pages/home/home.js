80a6d4df03f2b09a45fefd05d89a48079be7df24
928412257Hss <928412257@qq.com>
2020-06-17 15:09:44 +0800

    08-事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,81 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    titles:[' 衣服','裤子','鞋子']
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    console.log('按钮发生点击')
+  },
+  
+  handleTouchStart(){
+    console.log('handleTouchStart')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTouchMove(){
+    console.log('handleTouchMove')
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTouchEnd(){
+    console.log('handleTouchEnd')
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleTag(){
+    console.log('handleTag')
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleLongPress(){
+    console.log('handleLongPress')
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handleEventClick(event){
+    console.log('ddddddddd',event)
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  handleEventEnd(event){
+    console.log('+++++++++++++',event)
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  handleInner(event){
+    console.log('ssssssssssss',event)
+  },
 
-  }
+  handleOuter(event){
+    console.log('llllllllllll',event)
+  },
+
+  handleItemClick(event){
+    console.log(event)
+    const dataset = event.currentTarget.dataset
+    const title = dataset.item
+    const index = dataset.index
+    console.log(title, index)
+  },
+
+  // 事件冒泡和事件捕获
+  handleCaptureView1(){
+    console.log("handleCaptureView1")
+  },
+
+  handleTapView1(){
+    console.log("handleTapView1")
+  },
+
+  handleCaptureView2(){
+    console.log("handleCaptureView2")
+  },
+
+  handleTapView2(){
+    console.log("handleTapView1")
+  },
+
+  handleCaptureView3(){
+    console.log("handleCaptureView3")
+  },
+
+  handleTapView3(){
+    console.log("handleTapView3")
+  },
 })
+
