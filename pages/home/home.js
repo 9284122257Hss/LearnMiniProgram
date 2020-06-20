7962aba9acc060d7eb7644a145008afdcbf90b40
928412257Hss <928412257@qq.com>
2020-06-20 20:17:27 +0800

    11-展示弹窗和使用分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,65 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中ing',
+      duration:3000,
+      icon:'loading',
+      // image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592657873731&di=b82f528e7b3a3df9ff82445dce5c8f2d&imgtype=0&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D3843892960%2C4271081919%26fm%3D214%26gp%3D0.jpg'
+      mask:true,
+      success:function(){
+        console.log('展示弹窗成功')
+      },
+      fail:function(){
+        console.log('展示弹窗失败')
+      },
+      complete:function(){
+        console.log('完成函数调用')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title:'我是标题',
+      content:'我是内容哈哈哈',
+      success:function(res){
+        console.log(res)
+        if(res.cancel){
+          console.log('用户点击了取消按钮')
+        }
+        if(res.confirm){
+          console.log('用户点击了确定按钮')
+        }
+      },
+      // showCancel:false
+      cancelText:'退出',
+      cancelColor:'red'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading(){
+    wx.showLoading({
+      title: ' 加载ing',
+      mask:true
+    }),
+    setTimeout(() =>{
+      wx.hideLoading()
+    },2000)
+    
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleshowActionSheet(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor:'pink',
+      success:function(res){
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return {
+      title:'你好啊，陈正正',
+      path:'/pages/about/about',
+      imageUrl:'http://s11.mogucdn.com/mlcdn/c45406/180301_509b6ca365e3i1fiji2fk4kkaihd3_640x960.jpg_320x999.jpg'
+    }
   }
 })
