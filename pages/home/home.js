48811307f9484478976b34d01ae5e62a7ffb3ea6
928412257Hss <928412257@qq.com>
2020-06-20 17:58:47 +0800

    10-网络请求及其封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,54 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
+  data: {},
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // 1.原生发送网络请求
+    this.get_date_orign()
+    // 2.使用封装的request发送网络请求
+    request({
+      url:"http://152.136.185.210:8000/api/n3/home/multidata"
+    }).then(res =>{
+      console.log(res)
+    }).catch(err =>{
+      console.log(err)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_date_orign(){
+     // 发送网络请求
+    // 1.发送最简单的get请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/n3/home/multidata',
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // }),
+    // 2.get请求，但是携带参数
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/n3/home/data',
+    //   data:{
+    //     type:'pop',
+    //     page:1
+    //   },
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+    // 3.post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method:'post',
+    //   data:{
+    //     name:'czz',
+    //     age:18
+    //   },
+    //   success:function(res){
+    //     console.log(res)
+    //   },
+    //   fail:function(err){
+    //     console.log(err)
+    //   }
+    // })
   }
 })
